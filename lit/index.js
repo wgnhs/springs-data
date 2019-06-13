0541db154515241e2384fc33ccf0140658cf22c4
WGNHS Jenkins <devops@wgnhs.wisc.edu>
2019-06-13 21:52:58 +0000

    Updates

--- lit/index.js
+++ lit/index.js
@@ -77,31 +77,17 @@
   }
   customElements.define('in-radio', InRadio);
 
-  function renderTime() {
-    return new Date();
-  }
-
   class AppSidebar extends litElement.LitElement {
     static get properties() {
       return {
         title: {
           type: String
-        },
-        trackTime: {
-          type: Date,
-          attribute: false
         }
       };
     }
 
     constructor() {
       super();
-      this.renderTime = renderTime();
-      
-      // let track = (function() {
-      //   this.trackTime = renderTime();
-      // }).bind(this);
-      // setInterval(track, 1000);
     }
 
     static get styles() {
@@ -109,26 +95,10 @@
       :host {
         padding: 0 1.5em 1.5em 1.5em;
       }
-
-      .header {
-        position: -webkit-sticky;
-        position: sticky;
-        top: 0px;
-        background-color: var(--palette-white);
-        padding: 1em;
-      }
-
-      [data-element=views] {
-        display: grid;
-        grid-template-columns: 33% 33% 33%;
-      }
     `;
     }
 
     switchTab(choice) {
-      if (choice !== this.$views.choice) {
-        this.$views.choice = choice;
-      }
       this.shadowRoot.querySelectorAll('slot').forEach((el) => {
         if ((choice === 'default' && !el.getAttribute('name')) || (el.getAttribute('name') === choice)) {
           el.hidden = false;
@@ -149,33 +119,12 @@
       </style>
 
       <h1 class="header">${this.title}</h1>
-      <in-radio
-        data-element="views"
-        in-name="view"
-        choices='["default","filter","details"]'
-        @choice-change="${this.handleChoiceChange}"
-      ></in-radio>
       <slot></slot>
-      <slot name="filter" hidden></slot>
       <slot name="details" hidden></slot>
-      <div>Last refresh: ${this.renderTime}</div>
     `;
     }
-
-    firstUpdated() {
-      Object.assign(this, gatherElements(this.renderRoot, 'data-element'));
-    }
   }
 
-  function gatherElements(doc, attributeName) {
-    let result = {};
-    let elements = doc.querySelectorAll('[' + attributeName + ']');
-    for (let el of elements) {
-      let name = '$' + el.getAttribute(attributeName);
-      result[name] = el;
-    }
-    return result;
-  }
   customElements.define('app-sidebar', AppSidebar);
 
   /*!
@@ -200,6 +149,7 @@
       macro-carousel {
         padding: 0 3em;
         max-width: 75vw;
+        margin: 0 auto;
       }
       .slide {
         min-height: 10em;
@@ -490,6 +440,16 @@
       .detail {
         background-color: var(--palette-light);
       }
+
+      .header {
+        position: -webkit-sticky;
+        position: sticky;
+        top: 0px;
+        background-color: var(--palette-white);
+        padding: 1em;
+        z-index: 10;
+        width: 100%;
+      }
     `;
     }
 
@@ -500,7 +460,7 @@
       </style>
 
       ${(!this.siteinfo)? '' : litElement.html`
-        <h1>${this.siteinfo.County} County Spring #${this.siteinfo.SpringID}</h1>
+        <h1 class="header">${this.siteinfo.County} County Spring #${this.siteinfo.SpringID}</h1>
         <site-photos .photos="${this.photos}"></site-photos>
         <site-bed-materials .siteinfo="${this.siteinfo}"></site-bed-materials> 
         <div data-element="table">
