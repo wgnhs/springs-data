c3e51426cf35805e89244be36709505f9a1cdcad
WGNHS Jenkins <devops@wgnhs.wisc.edu>
2019-07-08 20:53:33 +0000

    Updates

--- lit/index.js
+++ lit/index.js
@@ -45,7 +45,7 @@
         @import url("./css/typography.css");
       </style>
 
-      <h1 class="header">${this.title}</h1>
+      ${(!this.title)?'':litElement.html`<h1 class="header">${this.title}</h1>`}
       <slot></slot>
       <slot name="details" hidden></slot>
     `;
@@ -758,9 +758,22 @@
         position: sticky;
         top: 0px;
         background-color: var(--palette-white);
-        padding: 1em;
+        padding: var(--font-size-extra-large);
         z-index: 10;
         width: 100%;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-between;
+      }
+      .header h1 {
+        padding: 0;
+      }
+      .header i {
+        display: flex;
+        align-items: center;
+        font-size: var(--font-size-extra-large);
+        color: var(--palette-accent);
+        cursor: pointer;
       }
     `;
     }
@@ -790,7 +803,10 @@
       </style>
 
       ${(!this.siteinfo)? '' : litElement.html`
-        <h1 class="header">${this.siteinfo.County} County Spring #${this.siteinfo.SpringID}</h1>
+        <div class="header">
+          <h1>${this.siteinfo.County} County Spring #${this.siteinfo.SpringID}</h1>
+          <i class="material-icons clear-selection" title="Clear selection" @click="${this.fireClearSelection}">clear</i>
+        </div>
         <site-photos .photos="${this.photos}"></site-photos>
         <slot name="sketch"></slot>
         <site-water-quality .siteinfo="${this.siteinfo}"></site-water-quality>
@@ -800,6 +816,14 @@
         </div>
       `}
     `;
+    }
+
+    fireClearSelection() {
+      let event = new CustomEvent('clear-selection', {
+        bubbles: true,
+        detail: {}
+      });
+      this.dispatchEvent(event);
     }
   }
   customElements.define('site-details', SiteDetails);
