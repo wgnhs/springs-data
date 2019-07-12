cce91248082ed3e997c64255f6fed31df65bd77e
WGNHS Jenkins <devops@wgnhs.wisc.edu>
2019-07-12 22:53:18 +0000

    Updates

--- lit/index.js
+++ lit/index.js
@@ -113,6 +113,10 @@
       color: var(--palette-900);
     }
 
+    .lbl-toggle:focus {
+      outline: thin dotted;
+    }
+
     .collapsible-content {
       max-height: 0px;
       overflow: hidden;
@@ -1271,19 +1275,48 @@
 
     static get styles() {
       return litElement.css`
+    .option-container {
+      box-sizing: border-box;
+      padding: var(--border-radius)
+    }
+    .icon {
+      font-size: var(--icon-size-extra-large);
+    }
     `;
     }
 
     render() {
       return litElement.html`
-      <div>
-        <h4>Map Point Styles:</h4>
-        <button @click="${this.normalPoints}">Normal Styling</button>
-        <button @click="${this.typePoints}">Style by Spring Type</button>
-        <button @click="${this.condPoints}">Style by Conductivity</button>
-        <button @click="${this.qPoints}">Style by Discharge</button>
+      <style>
+        @import url("./css/typography.css");
+      </style>
+      <h3>View sites by value:</h3>
+      <div class="option-container">
+        <map-control-item @click="${this.typePoints}">
+          <div slot="item-before"><span>Spring Type</span></div>
+          <div slot="item"></div>
+          <i slot="item-after" class="icon material-icons" title="View on map">map</i>
+        </map-control-item>
+        <map-control-item @click="${this.condPoints}">
+          <div slot="item-before"><span>Conductivity</span></div>
+          <div slot="item"></div>
+          <i slot="item-after" class="icon material-icons" title="View on map">map</i>
+        </map-control-item>
+        <map-control-item @click="${this.qPoints}">
+          <div slot="item-before"><span>Discharge</span></div>
+          <div slot="item"></div>
+          <i slot="item-after" class="icon material-icons" title="View on map">map</i>
+        </map-control-item>
+        <map-control-item @click="${this.normalPoints}">
+          <div slot="item-before">Reset</div>
+          <i slot="item-after" class="icon material-icons" title="View on map">clear</i>
+        </map-control-item>
       </div>
     `;
+    }
+
+    handleSelection(fn) {
+
     }
 
     _fire(eventName, detail) {
@@ -1307,6 +1340,91 @@
     }
   }
   customElements.define('map-controls', MapControls);
+
+  class MapControlItem extends litElement.LitElement {
+    static get properties() {
+      return {
+        selected: {
+          type: Boolean
+        }
+      };
+    }
+
+    constructor() {
+      super();
+      this.genId = genId();
+    }
+
+    static get styles() {
+      return litElement.css`
+
+    input[type='checkbox'] {
+      display: none;
+    }
+    .lbl-toggle {
+      display: block;
+      text-align: center;
+      cursor: pointer;
+    }
+    .lbl-toggle:hover {
+      color: var(--palette-900);
+    }
+    .lbl-toggle:focus {
+      outline: thin dotted;
+    }
+    .option-row {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+
+      background: var(--palette-white);
+      color: var(--palette-accent);
+      border: 1px solid var(--palette-light);
+      border-radius: var(--border-radius);
+      padding: var(--font-size);
+      margin: var(--border-radius) 0;
+    }
+    /* .toggle:checked + .option-row {
+      background: var(--palette-light);
+      color: var(--palette-900);
+    } */
+    .option-row > div {
+      display: flex;
+      align-items: center;
+    }
+    `;
+    }
+
+    render() {
+      return litElement.html`
+    <input id="${this.genId}" class="toggle" type="checkbox">
+    <label for="${this.genId}" class="lbl-toggle option-row" tabindex="0">
+      <!-- <div class="option-row"> -->
+        <div><slot name="item-before"></slot></div>
+        <div><slot name="item"></slot></div>
+        <div><slot name="item-after"></slot></div>
+      <!-- </div> -->
+    </label>
+    `;
+    }
+
+    firstUpdated() {
+      this.$input = this.renderRoot.querySelector('.toggle');
+      let myLabels = this.renderRoot.querySelectorAll('.lbl-toggle');
+
+      Array.from(myLabels).forEach(label => {
+        label.addEventListener('keydown', e => {
+          // 32 === spacebar
+          // 13 === enter
+          if (e.which === 32 || e.which === 13) {
+            e.preventDefault();
+            label.click();
+          }      });
+      });
+    }
+  }
+  customElements.define('map-control-item', MapControlItem);
 
   exports.AppCollapsible = AppCollapsible;
   exports.AppSidebar = AppSidebar;
