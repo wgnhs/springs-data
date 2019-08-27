64bfec33956d1b179de1782e12e9cb4be2f4288e
WGNHS Jenkins <devops@wgnhs.wisc.edu>
2019-08-27 15:53:25 +0000

    Updates

--- lit/index.js
+++ lit/index.js
@@ -1123,13 +1123,13 @@ a {
       text-decoration: none;
       margin: var(--border-radius) 0;
     }
-    .button-link:hover {
+    .link:hover {
       color: var(--palette-900);
     }
-    .button-link:focus {
+    .link:focus {
       outline: thin dotted;
     }
-    .button-link {
+    .link {
       display: block;
 
       font-weight: var(--font-weight-bold);
@@ -1157,7 +1157,7 @@ a {
 
     render() {
       return litElement.html`
-    <a class="button-link"
+    <a class="link"
       href="${this.href}"
       target="${this.target}"
       ?download=${this.download}>
